ea03053733f19902dad96e1bc8be551dfe5757a0
William Chen <exanite78012@gmail.com>
2026-02-28 09:52:50 -0500

    Bump bindings version

--- RegenerateBindings.ps1
+++ RegenerateBindings.ps1
@@ -10,12 +10,12 @@ $script = {
     nuke sln --projects Silk.NET.FreeType Silk.NET.HarfBuzz Silk.NET.Vulkan.Vma Silk.NET.Tracy Silk.NET.Slang
 
     # Clear outputs folder
-    Remove-Item -Path "build\output_packages/*" -Recurse -Force
+    Remove-Item -Path "build\output_packages\*" -Recurse -Force
 
     # Create NuGet packages for all bindings
     # Increment versions before running this script
-    nuke pack --Configuration Release --msbuild-properties VersionSuffix=Exanite-2
-    # nuke pack --Configuration Debug --msbuild-properties VersionSuffix=Exanite-2-Debug-1
+    nuke pack --Configuration Release --msbuild-properties VersionSuffix=Exanite-3
+    # nuke pack --Configuration Debug --msbuild-properties VersionSuffix=Exanite-3-Debug-1
 }
 
 Push-Location -Path $PSScriptRoot/submodules/Silk.NET
